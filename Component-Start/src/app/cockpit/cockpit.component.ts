918ac97662006885358ad9a2e6421c17961ff2ad
Karthik <56579449+karthik9728@users.noreply.github.com>
2022-05-05 11:58:04 +0530

    Final Changes in Components and data binding

--- Component-Start/src/app/cockpit/cockpit.component.ts
+++ Component-Start/src/app/cockpit/cockpit.component.ts
@@ -1,26 +1,39 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, DoCheck, ElementRef, EventEmitter, OnChanges, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
   templateUrl: './cockpit.component.html',
   styleUrls: ['./cockpit.component.css']
 })
-export class CockpitComponent implements OnInit {
+export class CockpitComponent implements OnInit ,OnChanges,DoCheck{
 
   // newServerName = '';
   //newServerContent = '';
+
+  //@ViewChild('serverContentInput',{static:true}) serverContentInput:ElementRef;
 
   @Output('srCreated') serverCreated = new EventEmitter<{serverName:string,serverContent:string}>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName:string,serverContent:string}>();
 
 
-  constructor() { }
+  constructor() {
+    console.log('Constructor');
+   }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges');
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
+  ngDoCheck(): void {
+    console.log('Do Check');
   }
 
   onAddServer(nameInput:HTMLInputElement,contentInput:HTMLInputElement) {
-    console.log(nameInput.value)
+    //console.log(nameInput.value)
     this.serverCreated.emit({serverName:nameInput.value,serverContent:contentInput.value});
   }
 
